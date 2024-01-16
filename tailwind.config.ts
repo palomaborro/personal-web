20d54ca66f0600c829a98ff01629c1ade7cd1615
Paloma Borro <paloma.borro@wizeline.com>
2024-01-16 13:55:31 +0100

    Add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "waikawa-gray": {
+          "50": "#f2f7fb",
+          "100": "#e7f0f8",
+          "200": "#d3e2f2",
+          "300": "#b9cfe8",
+          "400": "#9cb6dd",
+          "500": "#839dd1",
+          "600": "#6a7fc1",
+          "700": "#6374ae",
+          "800": "#4a5989",
+          "900": "#414e6e",
+          "950": "#262c40",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
