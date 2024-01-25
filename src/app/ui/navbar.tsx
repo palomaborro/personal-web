391cf0728507d1c06398e798efd46fe0d2b98204
palomaborro <palomabs93@gmail.com>
2024-01-25 14:40:38 +0100

    Add Navbar component

---
+++ src/app/ui/navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  const links = [
+    { name: "Resume", href: "/resume" },
+    { name: "Portfolio", href: "/portfolio" },
+    { name: "Contact", href: "/contact" },
+  ];
+  return (
+    <div className="flex items-center justify-center text-white space-x-4">
+      {links.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className="text-xl font-bold text-blue-violet-200 bg-transparent hover:text-blue-violet-50  focus:text-blue-violet-50 cursor-pointer"
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
