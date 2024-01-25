5905a479617e14cb9f4ce6de8ca962e9950e77eb
palomaborro <palomabs93@gmail.com>
2024-01-25 14:40:01 +0100

    Change font and add Navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import { montserrat } from "./ui/fonts";
+import { raleway } from "./ui/fonts";
 import "./ui/globals.css";
+import Navbar from "./ui/navbar";
 
 export default function RootLayout({
   children,
@@ -8,10 +9,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${montserrat.className} antialiased bg-waikawa-gray-800 text-white`}
-      >
+      <body className={`${raleway.className} antialiased bg-blue-violet-950`}>
+        <div className="py-6 flex justify-between items-center">
+          <span className="">
+            <p className="text-2xl font-bold text-blue-violet-200">
+              Sara Herráiz Gil
+            </p>
+            <p className="text-sm text-blue-violet-200">
+              Systems Biology and Bioinformatics
+            </p>
+          </span>
+          <Navbar />
+        </div>
         {children}
+        <footer className="text-white">
+          Made with love by Paloma Borro Santos while probably thinking about
+          drumming
+        </footer>
       </body>
     </html>
   );
